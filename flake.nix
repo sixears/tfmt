68da4bc29f132d7c86e75e15cfca1a7d86b5fe19
Mr. Fluffy <git@sixearsm>
2022-11-17 21:07:11 +0000

    add flake; use ghc-8.10.7 for tfmt

--- flake.nix
+++ flake.nix
@@ -3,12 +3,13 @@
 
   inputs = {
     nixpkgs.url      = "github:nixos/nixpkgs/be44bf67"; # nixos-22.05 2022-10-15
-    build-utils.url  = "github:sixears/flake-build-utils/r1.0.0.11";
+#    build-utils.url  = github:sixears/flake-build-utils/r1.0.0.11;
+build-utils.url  = path:/home/martyn/src/flake-build-utils;
 
-    base0t.url        = "github:sixears/base0t/r0.0.1.9";
-    has-callstack.url = "github:sixears/has-callstack/r1.0.1.11";
-    more-unicode.url  = "github:sixears/more-unicode/r0.0.17.8";
-    number.url        = "github:sixears/number/r1.1.2.10";
+    base0t.url        = github:sixears/base0t/r0.0.1.9;
+    has-callstack.url = github:sixears/has-callstack/r1.0.1.12;
+    more-unicode.url  = github:sixears/more-unicode/r0.0.17.8;
+    number.url        = github:sixears/number/r1.1.2.10;
   };
 
   outputs = { self, nixpkgs, build-utils
@@ -21,5 +22,30 @@
       # we're pinned to GHC 8 for now, because we're using text-format-0.3.2,
       # which has an upper bound on `base` of < 4.15 - ghc 9 uses >= 4.15.1
       ghc = p: p.ghc8107;
+
+      callPackage = { mkDerivation, lib, system
+                    , base, containers, data-textual, formatting, lens, parsers
+                    , prettyprinter, process, tasty, tasty-hunit
+                    , template-haskell, text, text-format, text-printer, time
+                    , trifecta
+                    }:
+        let
+          pkg = build-utils.lib.flake-def-pkg system;
+        in
+          mkDerivation {
+            pname = "tfmt";
+            version = "0.2.7.15";
+            src = ./.;
+            libraryHaskellDepends = [
+              base containers data-textual formatting lens parsers prettyprinter
+              process tasty tasty-hunit template-haskell text text-format
+              text-printer time trifecta
+            ] ++ map (p: pkg p) [ base0t has-callstack more-unicode number ];
+
+            testHaskellDepends = [ base tasty ];
+            description = "type-safe text/string formatting with a simple interface";
+            license = lib.licenses.mit;
+          };
+
     };
 }
