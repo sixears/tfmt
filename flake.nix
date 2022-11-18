6dbfb1d0a18e7c2d13744f4bb1cd72b43a53d278
Mr. Fluffy <git@sixearsm>
2022-11-18 18:43:38 +0000

    add flake; use ghc-8.10.7 for tfmt

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url      = github:nixos/nixpkgs/be44bf67; # nixos-22.05 2022-10-15
-    build-utils.url  = github:sixears/flake-build-utils/r1.0.0.11;
+    build-utils.url  = github:sixears/flake-build-utils/r1.0.0.12;
 
     base0t.url        = github:sixears/base0t/r0.0.1.9;
     has-callstack.url = github:sixears/has-callstack/r1.0.1.13;
@@ -14,9 +14,6 @@
   outputs = { self, nixpkgs, build-utils
             , base0t, has-callstack, more-unicode, number }:
     build-utils.lib.hOutputs self nixpkgs "tfmt" {
-      deps = {
-        inherit base0t has-callstack more-unicode number;
-      };
       unbreak = hpkgs: { inherit (hpkgs) text-format; };
       # we're pinned to GHC 8 for now, because we're using text-format-0.3.2,
       # which has an upper bound on `base` of < 4.15 - ghc 9 uses >= 4.15.1
@@ -39,7 +36,7 @@
               base containers data-textual formatting lens parsers prettyprinter
               process tasty tasty-hunit template-haskell text text-format
               text-printer time trifecta
-            ] ++ map (p: pkg p) [ base0t has-callstack more-unicode number ];
+            ] ++ map (p: pkg p) [ base0t has-callstack more-unicode number ] ;
 
             testHaskellDepends = [ base tasty ];
             description = "type-safe text/string formatting with a simple interface";
